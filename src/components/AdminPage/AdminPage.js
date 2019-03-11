16cfb2261a6317ea0ff3a5a2e31d1bfdf62f5d98
PatrickZ <pzarkha@hotmail.com>
2019-03-10 20:14:31 -0500

    added links and routes

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import ProjectPage from '../ProjectPage/ProjectPage'
 
 class AdminPage extends Component {
     state = {
@@ -60,13 +61,14 @@ class AdminPage extends Component {
                     <input type='text' placeholder="Name" value={this.state.newProject.name} onChange={this.handleNameChange('name')} />
                     <input type='text' placeholder="Description" value={this.state.newProject.description} onChange={this.handleNameChange('description')} />
                     <input type='text' placeholder="Thumbnail" value={this.state.newProject.thumbnail} onChange={this.handleNameChange('thumbnail')} />
-                    <input type='text' placeholder="Website" value={this.state.newProject.website} onChange={this.handleNameChange('website')} />
-                    <input type='text' placeholder="Github" value={this.state.newProject.github} onChange={this.handleNameChange('github')} />
+                    <input type='url' placeholder="Website" value={this.state.newProject.website} onChange={this.handleNameChange('website')} />
+                    <input type='url' placeholder="Github" value={this.state.newProject.github} onChange={this.handleNameChange('github')} />
                     <input type='date' placeholder="Date Completed" value={this.state.newProject.date_completed} onChange={this.handleNameChange('date_completed')} />
                     <input type='number' placeholder="Select a Tag" value={this.state.newProject.tag_id} onChange={this.handleNameChange('tag_id')} />
 
                     <Button type='submit' value='Add New Project' variant="contained" color="primary" > Add New</Button>
                 </form>
+                <ProjectPage />
             </div>
         );
     }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import ProjectPage from '../ProjectPage/ProjectPage'
 import AdminPage from '../AdminPage/AdminPage'
 import './App.css';
@@ -9,12 +10,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <div className="App-header">
-        <h1>Patrick Zarkha</h1>
-        <img src='images/goat_small.jpg' className="image" alt="broken" />
-      </div>
-        <ProjectPage />
-        <AdminPage />
+        <div>
+          <Router>
+            <Link to="/projects">Current Projects</Link>
+          </Router>
+        </div>
+        <Router>
+          <Link to="/admin">Admin Page</Link>
+        </Router>
+        <div className="App-header">
+          <h1>Patrick Zarkha</h1>
+          <img src='images/goat_small.jpg' className="image" alt="broken" />
+          <Router>
+            <div>
+              <Route path="/projects" component={ProjectPage} />
+              <Route path="/admin" component={AdminPage} />
+            </div>
+          </Router>
+        </div>
       </div>
     );
   }
